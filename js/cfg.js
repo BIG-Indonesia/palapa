82ef2511af8140df8e5aa80758715eeeedab223a
repobig <pusatppig@gmail.com>
2020-08-27 15:11:04 +0700

    update link basemap RBI

---
+++ js/cfg.js
@@ -0,0 +1,14 @@
+// Base Gsmanager API URL 
+var baseAPIURL = 'http://REPGSMANAGERDOM/api/';
+// Base Geoserver WMS URL
+var baseGSURL = 'http://REPGSMANAGERDOM/geoserver/wms';
+// Base Tile Layer for layer preview
+var baseXYZLayer = 'http://portal.ina-sdi.or.id/arcgis/rest/services/RBI/Basemap/MapServer/tile/{z}/{y}/{x}';
+// Base Center/Zoom for layer preview
+var previewCenter = {
+    lat: -2.5,
+    lon: 118,
+    zoom: 5,
+    projection: 'EPSG:4326',
+    bounds: []
+};
